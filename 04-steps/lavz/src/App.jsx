51a3f1cd9b201e1db1e0ad74eae1dac5dfca272e
Lucas Avanzi <lucas@1placeonline.com>
2025-01-05 21:21:08 -0400

    section 8 to 14 done

--- 04-steps/lavz/src/App.jsx
+++ 04-steps/lavz/src/App.jsx
@@ -1,60 +1,80 @@
 import { useState } from "react";
 
 const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
 ];
 
 export default function App() {
-  const [step, setStep] = useState(1);
-  const [showSteps, setShowSteps] = useState(true);
+    const [step, setStep] = useState(1);
+    const [showSteps, setShowSteps] = useState(true);
 
-  function nextStep() {
-    if (step < 3) {
-      setStep((s) => s + 1);
+    function nextStep() {
+        if (step < 3) {
+            setStep((s) => s + 1);
+        }
     }
-  }
 
-  function previousStep() {
-    if (step > 1) {
-      setStep((s) => s - 1);
+    function previousStep() {
+        if (step > 1) {
+            setStep((s) => s - 1);
+        }
     }
-  }
 
-  return (
-    <>
-      <button className="close" onClick={() => setShowSteps((is) => !is)}>
-        x
-      </button>
-      {showSteps && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
-
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-
-          <div className="buttons">
-            <button
-              onClick={previousStep}
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            >
-              Previous
+    return (
+        <>
+            <button className="close" onClick={() => setShowSteps((is) => !is)}>
+                x
             </button>
-            <button
-              onClick={nextStep}
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
+            {showSteps && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                    </div>
+
+                    <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+                    <div className="buttons">
+                        <Button
+                            bgColor="#7950f2"
+                            color="#fff"
+                            onClick={previousStep}
+                        >
+                            <span>👈</span> Previous
+                        </Button>
+                        <Button
+                            bgColor="#7950f2"
+                            color="#fff"
+                            onClick={nextStep}
+                        >
+                            Next <span>👉</span>
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+function StepMessage({ step, children }) {
+    return (
+        <p className="message">
+            <h3>Step {step}</h3>
+            {children}
+        </p>
+    );
+}
+
+function Button({ bgColor, color, onClick, children }) {
+    return (
+        <button
+            onClick={onClick}
+            style={{ backgroundColor: bgColor, color: color }}
+        >
+            {children}
+        </button>
+    );
 }
--- 05-travel-list/lavz/src/index.js
+++ 05-travel-list/lavz/src/index.js
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <App></App>
-  </StrictMode>
+    <StrictMode>
+        <App></App>
+    </StrictMode>
 );
